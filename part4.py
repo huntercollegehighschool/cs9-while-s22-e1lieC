8914ac3d44deed91aed2065dd01650b81138eefa
ellie chen <104437155+e1lieC@users.noreply.github.com>
2022-06-01 04:02:06 +0000

    assignment directions

--- part4.py
+++ part4.py
@@ -12,3 +12,14 @@ Enter a number or enter 0 to stop: 0
 Product: 60
 
 '''
+
+num_str = input("Enter a number or enter 0 to stop: ")
+num_int=int(num_str)
+total = 0
+
+
+while num_int != 0:
+  total = total*num_int
+  num = int(input("Enter a number or enter 0 to stop: "))
+    
+print("Product: ", total)
